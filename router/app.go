4e8e244a71ffcd18d5d2707ce2e420f949da91aa
dezhizhang <1541609448@qq.com>
2023-08-03 06:50:30 +0800

    feat:添加swagger

--- router/app.go
+++ router/app.go
@@ -1,14 +1,22 @@
 package router
 
 import (
+	docs "gin-gorm/docs"
 	"gin-gorm/service"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // Router 路由
 func Router() *gin.Engine {
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = ""
 
-	r.GET("/ping", service.Ping)
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/ping", service.Ping)
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	return r
 }
--- service/ping.go
+++ service/ping.go
@@ -7,6 +7,18 @@ import (
 	"net/http"
 )
 
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
+
 func Ping(ctx *gin.Context) {
 	var user model.User
 	dao.DB.Where("id=?", 123).Find(&user)
