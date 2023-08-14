eca305d1d3beb611f1e3edf0a603193ac7b8002d
dezhizhang <1541609448@qq.com>
2023-08-15 07:59:00 +0800

    feat:修改报错

--- main.go
+++ main.go
@@ -1,10 +1,47 @@
 package main
 
-import "gin-gorm/router"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
 
 func main() {
-	engine := router.Router()
+	url := "https://api.foolstack.net/v1/poster"
 
-	engine.Run()
+	payload := map[string]interface{}{
+		"url": "https://foolstack.net",
+		"option": map[string]interface{}{
+			"type":      "png",
+			"width":     320,
+			"height":    480,
+			"waitUntil": "networkidle0",
+			"quality":   100,
+			"fullPage":  "true",
+		},
+	}
 
+	jsonPayload, err := json.Marshal(payload)
+
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
+
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsa3Q5Yjh1ZTAwMmFweWV6bXR6a2h4b3EiLCJzZXJ2aWNlIjoiUE9TVEVSIiwiaWF0IjoxNjkwOTUyNDQ2LCJleHAiOjQ4NDY3MTI0NDZ9._SJsQhUnHhsBXAm-Dw1DxzwbknHRVjjyjbOBZAUNPlc")
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
 }
